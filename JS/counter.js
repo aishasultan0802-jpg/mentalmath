74cfa5c71bed039005bfa22a1b1ae45ba56f1456
aishasultan0802-jpg <aisha.sultan0802@gmail.com>
2026-09-18 01:16:27 +0400

    Update counter.js

--- JS/counter.js
+++ JS/counter.js
@@ -1,7 +1,6 @@
 
 (function () {
   const COUNTER_URL = "https://script.google.com/macros/s/AKfycbwkhm2FhQbgdc18UpAotjOZ2vbbEALErgCmnp1FQhTh3U55pXVruKKq_TTCqPMBi2J83w/exec";
-
   const COUNT_ELEMENT_ID = "participantCount";
   const STORAGE_KEY = "sifahi_hesablama_visitor_id";
   const COUNT_CACHE_KEY = "sifahi_hesablama_count";
